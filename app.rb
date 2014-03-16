b815f91a5ba9492cf1a5c1679574974113110c15
Rafael B. Tauil <rafael@tauil.com.br>
2014-03-16 03:01:56 -0300

    Fixes montage rmagick syntax and adds back gravatars image list output and also generated montage file

--- app.rb
+++ app.rb
@@ -13,17 +13,19 @@ get '/image' do
     '8f9968630845dc22fffdb581c54324e8',
     '0c55140c23bc76e128764feac26edb71',
     'b5213ca6d34dc9e13a8ef265d1835116',
-    'de89dceb54a28b2aa952663cc94d60fc',
-    '4bf6b0cbd28f97183a2490671e257abf'
+    '4bf6b0cbd28f97183a2490671e257abf',
+    '06e4c84000579d88c9298a6616a42e29'
   ]
 
   code = []
   image = Magick::ImageList.new
 
   gravatars.each do |gravatar|
+    code << "<img src='http://www.gravatar.com/avatar/#{gravatar}' />"
     image.from_blob(open("http://www.gravatar.com/avatar/#{gravatar}").read)
   end
 
-  image.montage.frame = Magick::Geometry.new(80,80,0,0)
-  image.montage.write("./teste1.jpg")
+  image.montage{self.geometry = "+0+0"}.write("./generated_gravatars_montage.jpg")
+
+  erb code.join('')
 end
